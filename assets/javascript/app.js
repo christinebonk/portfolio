4368631060f81ba01a3a6e4def12ffcaf5db398b
Christine Bonk <christinebonk@gmail.com>
2018-06-10 22:17:02 -0400

    added images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,43 @@
 var portfolio = [
 	{
 		title: "Word Guess Game",
-		subtitle: "HTML, CSS, Javascript",
-		description: "This program uses HTML, CSS and Javscript to make a simple hangman game. Users are presented with unknown words that they must guess, letter by letter, until the hangman drawing is complete.",
-		link: "https://christinebonk.github.io/Word-Guess-Game/"
+		subtitle: "HTML5, CSS, Javascript",
+		description: "A simple word guess game, where users are presented with unknown horror movies that they must guess, letter by letter, before the hangman drawing is complete.",
+		link: "https://christinebonk.github.io/Word-Guess-Game/",
+		glink: "https://github.com/christinebonk/Word-Guess-Game",
+		src: "assets/images/hangman.png"
 	},
 	{
-		title: "Rock, Paper, Scissors",
-		subtitle: "HTML, CSS, Javascript, Firebase",
-		description: "Using firebase as a database, this rock paper scissors game allows two players to have a go at this classic game.",
-		link: "https://christinebonk.github.io/RPS-Multiplayer/"
+		title: "Multiplayer RPS",
+		subtitle: "HTML5, CSS, Javascript, Firebase",
+		description: "A two-player game where opponents must compete to becomes the rock, paper, scissors champions",
+		link: "https://christinebonk.github.io/RPS-Multiplayer/",
+		glink: "https://github.com/christinebonk/RPS-Multiplayer",
+		src: "assets/images/rps.png"
 	},
 	{
 		title: "Pokeman Game",
-		subtitle: "HTML, CSS, JQuery",
-		description: "A simple JQuery game where a user can choose a pokemon to go into battle against other pokemon. You must defeat all enemy pokemon to win the game.",
-		link: "https://christinebonk.github.io/unit-4-game/"
+		subtitle: "HTML5, CSS, JQuery",
+		description: "A simple JQuery game where a user can choose a pokemon to go into battle against other pokemon. The player must defeat all enemy pokemon to win the game.",
+		link: "https://christinebonk.github.io/unit-4-game/",
+		glink: "https://github.com/christinebonk/unit-4-game",
+		src: "assets/images/pokemon.png"
 	},
 	{
 		title: "Japan Trivia",
-		subtitle: "HTML, CSS, Javascript",
-		description: "A timed Japan Trivia game where the user is given 10 seconds to answer each question.",
-		link: "https://christinebonk.github.io/Trivia-Game/"
+		subtitle: "HTML5, CSS, JQuery",
+		description: "A timed Trivia game where the user is given 10 seconds to answer each question that tests their knowledge on Japan.",
+		link: "https://christinebonk.github.io/Trivia-Game/", 
+		glink: "https://github.com/christinebonk/Trivia-Game",
+		src: "assets/images/trivia.png"
 	},
 	{
 		title: "CryptoAdvi",
-		subtitle: "HTML, CSS, Javascript, APIs",
-		description: "This program uses HTML, CSS and Javscript to make a simple hangman game. Users are presented with unknown words that they must guess, letter by letter, until the hangman drawing is complete.",
-		link: "https://christinebonk.github.io/crypto-website/"
-	},
-	{
-		title: "Gifquarium",
-		subtitle: "HTML, CSS, Javascript",
-		description: "This program uses HTML, CSS and Javscript to make a simple hangman game. Users are presented with unknown words that they must guess, letter by letter, until the hangman drawing is complete.",
-		link: "https://christinebonk.github.io/giftastic/"
+		subtitle: "HTML5, CSS, JQuery, APIs",
+		description: "An investment research tool that consolidates information on crypto currencies that can be traded in Canadian dollars",
+		link: "https://christinebonk.github.io/crypto-website/",
+		glink: "https://github.com/christinebonk/crypto-website",
+		src: "assets/images/crypto.png"
 	}
 ];
 
@@ -52,6 +56,8 @@ function updatePortfolio(index) {
 	$("#portfolio-subtitle").text(portfolio[index].subtitle);
 	$("#portfolio-description").text(portfolio[index].description);
 	$("#portfolio-button").attr("href", portfolio[index].link);
+	$("#github-button").attr("href", portfolio[index].glink);
+	$("#portfolio-image").attr("src", portfolio[index].src)
 }
 
 updatePortfolio(0);
